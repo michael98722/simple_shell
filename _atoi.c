34bf1efb7884e5d5040f7ef85b1d54c965216d54
Abishe1 <abisheguma@gmail.com>
2022-12-11 10:22:55 -0800

    project

--- _atoi.c
+++ _atoi.c
@@ -8,6 +8,7 @@ int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 /**
  * is_delim - checks if character is a delimeter
  * @c: the char to check
@@ -16,16 +17,36 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+
+int _isalpha(int c)
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
+
+
 /**
  *_atoi - converts a string to an integer
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
+
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
